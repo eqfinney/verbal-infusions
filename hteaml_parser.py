9a364be0f995bb1805ec9e1558bc2dad1f4f8175
Emily Quinn Finney <eqfinney@ucdavis.edu>
2017-10-05 11:35:58 -0400

    final refactoring touches

--- hteaml_parser.py
+++ hteaml_parser.py
@@ -35,10 +35,13 @@ def read_soup(filename):
     return structured_pages
 
 
-def locate_descriptive_text(filename, structured_page, tag_pattern, tag_names):
+def locate_descriptive_text(structured_page, tag_pattern, tag_names, filename):
         """
         Given a page structured in a Beautiful Soup format, returns all descriptive text on page
         :param structured_page: list of web pages, structured in Beautiful Soup format
+        :param tag_pattern: string, the HTML tag pattern for which to search
+        :param tag_names: tuple of strings, the names of the relevant HTML tags to write
+        :param filename: string, the name of the file to which to write the text
         :return: nothing, but should write a corpus of text to file
         """
         with open(filename, 'a') as f:
@@ -60,38 +63,3 @@ def locate_descriptive_text(filename, structured_page, tag_pattern, tag_names):
                                     f.write(child.string)
                 else:
                     pass
-
-
-def find_id(url, id_sequence):
-    """
-    Matches the identification sequence in a URL, returning the ID number in the URL
-    :param url: a URL from which to draw a product ID number
-    :param id_sequence: the identification sequence used to ID products in a URL
-    :return: id_number, the ID number of the product referenced in the URL
-    """
-    # find the parts of the string that match id_sequence
-    if re.search(id_sequence, url):
-        id_number = re.search(id_sequence, url).group()
-    else:
-        id_number = None
-    return id_number
-
-
-def identify_duplicates(url, master_list, id_sequence):
-    """
-    Determines whether the product has already been included in a master list of products.
-    :param url: a URL from which to draw a product ID number
-    :param master_list: the master list of products with which to compare the product ID
-    :param id_sequence: the ID sequence used to ID products in a URL
-    :return: a Boolean showing whether the product has been seen (T) or not (F)
-    """
-    id_number = find_id(url, id_sequence)
-    if id_number:
-        # check that ID against the master_list
-        if id_number in master_list:
-            return True
-        else:
-            return False
-    # if no ID number, treat the page as a duplicate and don't add it to the list
-    else:
-        return True
