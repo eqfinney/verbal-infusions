4ccf5e50730d59d6e7cc2d5f714fbc2de45380bf
Emily Quinn Finney <eqfinney@ucdavis.edu>
2017-10-16 13:26:52 -0400

    added gitignore

--- test_hteaml_parser.py
+++ test_hteaml_parser.py
@@ -15,15 +15,3 @@ from bs4 import BeautifulSoup
 def test_read_soup(filename='tea_corpus.html'):
     file = hp.read_soup(filename)
     assert type(file[0]) == type(BeautifulSoup('', 'lxml'))
-
-
-def test_find_id(url='http://shop.numitea.com/NUMIS-10430&c=NumiTeaStore@ByType@Puerh',
-                 id_sequence='NUMIS-[0-9]*'):
-    value = hp.find_id(url, id_sequence)
-    assert value == 'NUMIS-10430'
-
-
-def test_identify_duplicates(url='http://shop.numitea.com/NUMIS-10430&c=NumiTeaStore@ByType@Puerh',
-                             master_list={'NUMIS-10430'}, id_sequence='NUMIS-[0-9]*'):
-    truefalse = hp.identify_duplicates(url, master_list, id_sequence)
-    assert truefalse
